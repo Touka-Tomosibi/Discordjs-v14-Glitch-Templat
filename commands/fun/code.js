218b097abb7fd4e5ab331c9887acfbfd87b9592e
Glitch (discordjs-v14-node-v16-templates) <none>
2024-03-24 19:06:56 +0000

    🏰🐑 Updated with Glitch

--- commands/fun/code.js
+++ commands/fun/code.js
@@ -9,7 +9,7 @@ module.exports = {
     
 	async execute(interaction) {
   
-		await interaction.reply('[Glitch](https://glitch.com/edit/#!/candy-knowing-mass)\n[Github](https://github.com/Touka-Tomosibi/Discordjs-v14-Templat/tree/glitch)'); //送信される内容
+		await interaction.reply('[Glitch](https://glitch.com/edit/#!/discordjs-v14-node-v16-templates)\n[Github](https://github.com/Touka-Tomosibi/Discordjs-v14-Templat/tree/glitch)'); //送信される内容
     
 	},
 };
--- events/code.js
+++ events/code.js
@@ -7,7 +7,7 @@ module.exports = {
         
         if (message.content == 'コード教えて') { //反応するメッセージ(完全一致)     
 
-            await message.channel.send('[Glitch](https://glitch.com/edit/#!/candy-knowing-mass)\n[Github](https://github.com/Touka-Tomosibi/Discordjs-v14-Templat/tree/glitch)');//送信するメッセージ
+            await message.channel.send('[Glitch](https://glitch.com/edit/#!/discordjs-v14-node-v16-templates)\n[Github](https://github.com/Touka-Tomosibi/Discordjs-v14-Templat/tree/glitch)');//送信するメッセージ
             
          }
 	},
